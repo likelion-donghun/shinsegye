f3aafc703af11b32bb2349506fd3dec5dc9b0969
Shin <136shin@gmail.com>
2017-03-23 17:36:39 +0900

    complete test_ensemble

--- test_ensemble.py
+++ test_ensemble.py
@@ -1,5 +1,20 @@
 from sklearn.metrics import roc_curve
 from sklearn.metrics import auc
+from ensemble_total import *
+import matplotlib.pyplot as plt
+
 colors = ['black', 'orange', 'blue', 'green']
 linestyles = [':', '--', '-.', '-' ]
 for clf, label, clr, ls in zip(all_clf, clf_labels, colors, linestyles):
+    y_pred = clf.fit(X_train, y_train).predict_proba(X_test)[:, 1]
+    fpr, tpr, thresholds = roc_curve(y_true=y_test, y_score=y_pred)
+    roc_auc = auc(x=fpr, y=tpr)
+    plt.plot(fpr, tpr, color=clr, linestyle=ls, label='%s (aus = %0.2f)'%(label, roc_auc))
+plt.legend(loc='upper left')
+plt.plot([0, 1], [0, 1], linestyle='--', color = 'gray', linewidth=2)
+plt.xlim([-0.1, 1.1])
+plt.ylim([-0.1, 1.1])
+plt.grid()
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.show()
