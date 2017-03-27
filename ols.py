c69bfda7e5c0d3b0b172ff9ccc6b642bcc8da712
Shin <136shin@gmail.com>
2017-03-28 08:20:27 +0900

    lin_reg_housing(by creating linregGD)

---
+++ ols.py
@@ -0,0 +1,63 @@
+import numpy as np
+import pandas as pd
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/housing/housing.data', header=None, sep='\s+')
+df.columns = ['CRIM','ZN','INDUS','CHAS','NOX','RM','AGE','DIS','RAD','TAX','PTRATIO','B','LSTAT','MEDV']
+import matplotlib.pyplot as plt
+
+class LinearRegressionGD(object):
+    def __init__(self, eta=0.001, n_iter=20):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, X, y):
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            output = self.net_input(X)
+            errors = (y - output)
+            self.w_[1:] +=  self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors**2).sum()/2.0
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:])+self.w_[0]
+
+    def predict(self, X):
+        return self.net_input(X)
+
+
+X = df[['RM']].values
+y = df['MEDV'].values
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+sc_y = StandardScaler()
+X_std = sc_x.fit_transform(X)
+y_std = sc_y.fit_transform(y)
+lr = LinearRegressionGD()
+lr.fit(X_std, y_std)
+
+plt.plot(range(1, lr.n_iter+1), lr.cost_)
+plt.ylabel('SSE')
+plt.xlabel('Epoch')
+plt.show()
+
+
+# 선형회귀 모델의 피팅 수준 시각화
+def lin_regplot(X, y, model):
+    plt.scatter(X, y, c='blue')
+    plt.plot(X, model.predict(X), color='red')
+    return None
+
+lin_regplot(X_std, y_std, lr)
+plt.xlabel('Average number of rooms [RM] (standardized)')
+plt.ylabel('Price in $1000\'s [MEDV] (standardized)')
+plt.show()
+
+# 출력 변수의 스케일 복원
+num_rooms_std = sc_x.transform([5.0]) #방 5개를 스케일링
+price_std = lr.predict(num_rooms_std) #방 5개의 가격 예측
+print("Print in $1000's: %.3f" % sc_y.inverse_transform(price_std))
+print("Print in scale: %.3f" % price_std)
